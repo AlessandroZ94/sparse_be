4a262d01208e9694a23a851408459a448ef2e528
Alessandro Zecchi <alessandro.zecchi94@gmail.com>
2025-03-12 17:33:14 +0100

    comment

--- tests/test_sparse_be_matrix.py
+++ tests/test_sparse_be_matrix.py
@@ -33,29 +33,29 @@ class Test_TestBlockEncodedMatrix(unittest.TestCase):
         np.testing.assert_almost_equal(matrix, initial_matrix*factor, decimal=16) # equal matrices up to 16 decimal places
 
     def test_full_be_matrix_2(self):
-            # generate a matrix and block encode it
-            n = 1
-            N = 2**n
-            alpha = 1/2
-            beta = 1/8
-            gamma = 1/8
-            A = create_banded_circulant_matrix(n, alpha, beta, gamma)
-            #The block encoding will rescale the matrix by a factor of 1/2
-            factor=1/2
+        # generate a matrix and block encode it
+        n = 1
+        N = 2**n
+        alpha = 1/2
+        beta = 1/8
+        gamma = 1/8
+        A = create_banded_circulant_matrix(n, alpha, beta, gamma) # symmetric 2x2 case
+        #The block encoding will rescale the matrix by a factor of 1/2
+        factor=1/2
 
-            initial_matrix = np.array(A, dtype=complex)
-            print("\n")
+        initial_matrix = np.array(A, dtype=complex)
+        print("\n")
 
-            print("\n")
-            circ, alpha = be(A, draw=True)
+        print("\n")
+        circ, alpha = be(A, draw=True)
 
-            op = qi.Operator(circ)
-            circ_matrix = op.data
-            matrix = circ_matrix[0:N,0:N]
-     
-            print("\n")
+        op = qi.Operator(circ)
+        circ_matrix = op.data
+        matrix = circ_matrix[0:N,0:N]
+    
+        print("\n")
 
-            np.testing.assert_almost_equal(matrix, initial_matrix*factor, decimal=16) # equal matrices up to 16 decimal places
+        np.testing.assert_almost_equal(matrix, initial_matrix*factor, decimal=16) # equal matrices up to 16 decimal places
 
  
 if __name__ == '__main__':
